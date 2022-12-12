e16d7965949ce791bc254b433c154c1f590ecef5
AnaMariaTozi <anamariatozi@gmail.com>
2022-12-11 23:06:01 -0300

    add dotenv nuget

---
+++ src/GeoPetApi.Test/UnitTest1.cs
@@ -0,0 +1,17 @@
+using Xunit;
+using FluentAssertions;
+
+namespace GeoPetApi.Test;
+
+public class UnitTest1
+{
+	// [Theory(DisplayName = "Deve retornar \"Olá\"")]
+	// [InlineData("Olá")]
+	[Fact]
+	public void Test1()
+	{
+		// var result = Class1.Greet();
+
+        // result.Should().Be(expected);
+	}
+}
