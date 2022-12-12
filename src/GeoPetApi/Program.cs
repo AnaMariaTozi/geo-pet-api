6c50d4a245962bcd24f9dbfd73e4b4cd3c634f87
AnaMariaTozi <anamariatozi@gmail.com>
2022-12-12 20:52:26 -0300

    build docker and sql db

--- src/GeoPetApi/Program.cs
+++ src/GeoPetApi/Program.cs
@@ -1,9 +1,19 @@
+// using mentoria_api.Repository;
+using System.Text.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddMvcCore();
+builder.Services.AddControllersWithViews();
+// builder.Services.AddDbContext<BookStoreContext>();
+// builder.Services.AddScoped<IBookStoreContext, BookStoreContext>();
+// builder.Services.AddScoped<IBookStoreRepository, BookStoreRepository>();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddControllersWithViews()
+                .AddJsonOptions(x => x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
